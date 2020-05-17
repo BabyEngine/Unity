0a507d6517934ef71e3675a31d9f2aaeb5ddad3f
blake <ade.li.indie@gmail.com>
2020-05-18 00:17:12 +0800

    注释

--- Assets/BabyEngine/lua/framework/event/manager.lua
+++ Assets/BabyEngine/lua/framework/event/manager.lua
@@ -1,17 +1,24 @@
 EventManager = {}
 local events = {}
+local id = 1
 
+-- @luadoc 添加监听器
+-- @params target  any      触发事件时, 传递的首个参数
+-- @params event   string   事件名称
+-- @params handler function 回调
+-- @return int 事件索引
 function EventManager.AddListener( target, event, handler )
     if not event or type(event) ~= 'string' then
-        error('event type error type should be string(' .. type(event) .. ')')
+        print('event type error type should be string(' .. type(event) .. ')')
     end
     if not handler or type(handler) ~= 'function' then
-        error('handler type error type should be function(' .. type(handler) .. ')')
+        print('handler type error type should be function(' .. type(handler) .. ')')
     end
 
     if not events[event] then
         local evt = {}
         evt.observers = {}
+        evt.count = 0
         function evt.Dispatch( ... )
             for k,v in pairs(evt.observers) do
                 v.handler(v.target, ...)
@@ -22,39 +29,47 @@ function EventManager.AddListener( target, event, handler )
                 target  = target,
                 handler = handler
             }
-            evt.observers[obj] = obj
+            local ref = id
+            evt.observers[ref] = obj
+            evt.count = evt.count + 1
+            id = id + 1
+            return ref
         end
-        function evt.RemoveListener(target, handler)
-            for k,v in pairs(evt.observers) do
-                if v.target == target and v.handler == handler then
-                    evt.observers[k] = nil
-                    return
-                end
+        function evt.RemoveListener(ref)
+            if evt.observers[ref] then
+                evt.observers[ref] = nil
+                evt.count = evt.count - 1
+            end
+            if evt.count == 0 then
+                events[event] = nil
             end
         end
         events[event] = evt
     end
-    events[event].AddListener(target, handler)
+    return events[event].AddListener(target, handler)
 end
 
-function EventManager.RemoveListener(target, event, handler)
-    if event == nil then
-        return
-    end
+-- @luadoc 移除监听器
+-- @params event string 事件名称
+-- @params ref   int    索引
+function EventManager.RemoveListener(event, ref)
     if not events[event] then
+        print('no such event:', event)
         return
     end
-
-    events[event].RemoveListener(target, handler)
+    events[event].RemoveListener(ref)
 end
 
+-- @luadoc 触发事件
+-- @params event string 事件名称
+-- @params ...   args   参数
 function EventManager.Dispatch(event, ... )
     if type(event) ~= 'string' then
-        error('Dispatch Event Args Error(' .. type(event) .. ')')
+        print('Dispatch Event Args Error(' .. type(event) .. ')')
     end
     if events[event] then
         events[event].Dispatch(...)
     else
-        error('no such event:' .. event)
+        print('no such event:' .. event)
     end
 end
